e430bcb5f9ea3cdc54a5187cb4551c56230dc80c
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-03-18 23:31:26 -0400

    removed lda

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css'
 
 const sentiment = new Sentiment()
 
+// Returned Objects
+// Score: Score calculated by adding the sentiment values of recognized words.
+// Comparative: Comparative score of the input string.
+// Calculation: An array of words that have a negative or positive valence with their respective AFINN score.
+// Token: All the tokens like words or emojis found in the input string.
+// Words: List of words from input string that were found in AFINN list.
+// Positive: List of positive words in input string that were found in AFINN list.
+// Negative: List of negative words in input string that were found in AFINN list.
+
 
 class App extends Component {
 
@@ -38,6 +47,11 @@ class App extends Component {
     }
     console.log(result)
   }
+
+  findTopics(event) {
+    
+  }
+
 
   render() {
   return (
